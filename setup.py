a8459dc686417b698a27ddbd272303c4087b7a67
Frederik Kemner <stuff+github@fredo.org>
2023-05-11 08:18:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,49 @@
-from setuptools import setup, find_packages
+# coding=utf-8
+''' Plugin setup file '''
 
-setup(
-    name="Coordinate Systems",
-    version="0.1",
-    description="An OctoPrint plugin to manage workspace coordinate systems (G54-G59.3) for Marlin-based CNC machines",
-    author="Frederik Kemner",
-    author_email="octoprint-coordinate-systems@fredo.org",
-    url="https://github.com/040medien/octoprint-coordinate-systems",
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        "OctoPrint>=1.8.0",
-    ],
-    entry_points={
-        "octoprint.plugin": [
-            "coordinate_systems = octoprint_coordinate_systems"
-        ],
-    },
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
-        "Framework :: Flask",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Manufacturing",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Printing",
-        "Topic :: System :: Networking :: Monitoring",
-    ],
+from setuptools import setup
+plugin_identifier = 'coordinatesystems'
+plugin_package = 'octoprint_coordinatesystems'
+plugin_name = 'OctoPrint-CoordinateSystems'
+plugin_version = '0.1'
+plugin_description = 'An OctoPrint plugin to manage workspace coordinate systems (G54-G59.3) for Marlin-based CNC machines'
+plugin_author = 'Frederik Kemner'
+plugin_author_email = 'coordinate-systems@fredo.org'
+plugin_url = 'https://github.com/040medien/octoprint-coordinate-systems'
+plugin_license = 'Apache License 2.0'
+plugin_requires = ['OctoPrint>=1.8.0']
+plugin_additional_data = []
+plugin_additional_packages = []
+plugin_ignored_packages = []
+additional_setup_parameters = {}
+
+try:
+    import octoprint_setuptools
+except ImportError:
+    print('Could not import OctoPrint\'s setuptools, are you sure you are ' +
+          'running that under the same python installation that OctoPrint ' +
+          'is installed under?')
+    import sys
+    sys.exit(-1)
+
+setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
+    identifier=plugin_identifier,
+    package=plugin_package,
+    name=plugin_name,
+    version=plugin_version,
+    description=plugin_description,
+    author=plugin_author,
+    mail=plugin_author_email,
+    url=plugin_url,
+    license=plugin_license,
+    requires=plugin_requires,
+    additional_packages=plugin_additional_packages,
+    ignored_packages=plugin_ignored_packages,
+    additional_data=plugin_additional_data
 )
 
+if additional_setup_parameters:
+    from octoprint.util import dict_merge
+    setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
+
+setup(**setup_parameters)
