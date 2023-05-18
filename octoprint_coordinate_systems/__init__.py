c0042de81d22dd6ab37d42821ddf8492b857824b
Frederik Kemner <stuff+github@fredo.org>
2023-05-18 22:12:35 +0200

    CRUD position, offset, labels

--- octoprint_coordinate_systems/__init__.py
+++ octoprint_coordinate_systems/__init__.py
@@ -1,83 +1,79 @@
 import octoprint.plugin
 from octoprint.events import Events
+from octoprint.settings import NoSuchSettingsPath
 
 class CoordinateSystemsPlugin(octoprint.plugin.StartupPlugin,
                               octoprint.plugin.TemplatePlugin,
-                              octoprint.plugin.SettingsPlugin,
                               octoprint.plugin.AssetPlugin,
-                              octoprint.plugin.SimpleApiPlugin):
+                              octoprint.plugin.EventHandlerPlugin,
+                              octoprint.plugin.SettingsPlugin):
 
-    def __init__(self):
-        pass
+    # Other methods...
 
-    ## StartupPlugin mixin
+    def save_offsets(self, system, xOffset, yOffset, zOffset, label):
+        self._settings.set(["offsets", system], {"x": xOffset, "y": yOffset, "z": zOffset, "label": label})
+        self._settings.save()  # save the settings to file
 
-    def on_after_startup(self):
-        self._logger.info("Coordinate Systems Plugin started")
-
-    ## TemplatePlugin mixin
-
-    def get_template_configs(self):
-        return [
-            {
-                "type": "tab",
-                "custom_bindings": True,
-                "template": "coordinate_systems_tab.jinja2",
-                "name": "Coordinate Systems"
-            }
-        ]
-
-    ## SettingsPlugin mixin
-
-    def get_settings_defaults(self):
-        return dict(offsets={})
-
-    ## AssetPlugin mixin
-
-    def get_assets(self):
-        return dict(
-            js=["js/coordinate_systems.js"],
-            css=["css/coordinate_systems.css"]
-        )
-
-    ## SimpleApiPlugin mixin
-
-    def get_api_commands(self):
-        return {
-            "set_offsets": ["offsets"]
-        }
-
-    def on_api_command(self, command, data):
-        if command == "set_offsets":
-            offsets = data.get("offsets", None)
-            if offsets is not None:
-                self.set_offsets(offsets)
+    def load_offsets(self, system):
+        try:
+            return self._settings.get(["offsets", system])
+        except NoSuchSettingsPath:
+            return {"x": 0, "y": 0, "z": 0, "label": ""}  # return default offsets and label if none exist
 
     def set_offsets(self, system, xOffset, yOffset, zOffset):
-            # Send G53 to switch to machine coordinates
-            self._printer.commands("G53")
-    
-            # Request current machine coordinates with M114
-            self._printer.commands("M114")
-    
-            # Define a callback to handle the 'PositionUpdate' event
-            def on_position_update(event, payload):
-                # Calculate new positions based on the desired offsets
-                newX = payload["x"] - xOffset
-                newY = payload["y"] - yOffset
-                newZ = payload["z"] - zOffset
-    
-                # Switch to the desired workspace coordinate system
-                self._printer.commands(system)
-    
-                # Set new positions for the workspace coordinate system
-                self._printer.commands("G92 X{} Y{} Z{}".format(newX, newY, newZ))
-    
-                # Unsubscribe from the 'PositionUpdate' event
-                self._event_bus.unsubscribe(Events.POSITION_UPDATE, on_position_update)
-    
-            # Subscribe to the 'PositionUpdate' event
-            self._event_bus.subscribe(Events.POSITION_UPDATE, on_position_update)
+        # Switch to machine coordinates
+        self._printer.commands("G53")
+
+        # Request current machine coordinates with M114
+        self._printer.commands("M114")
+
+        # Define a callback to handle the 'PositionUpdate' event
+        def on_position_update(event, payload):
+            # Calculate new positions based on the desired offsets
+            newX = payload["x"] - xOffset
+            newY = payload["y"] - yOffset
+            newZ = payload["z"] - zOffset
+
+            # Switch to the desired workspace coordinate system
+            self._printer.commands(system)
+
+            # Reset the workspace coordinate system to machine zero
+            self._printer.commands("G92.1")
+
+            # Set new positions for the workspace coordinate system
+            self._printer.commands("G92 X{} Y{} Z{}".format(newX, newY, newZ))
+
+            # Unsubscribe from the 'PositionUpdate' event
+            self._event_bus.unsubscribe(Events.POSITION_UPDATE, on_position_update)
+
+        # Subscribe to the 'PositionUpdate' event
+        self._event_bus.subscribe(Events.POSITION_UPDATE, on_position_update)
+
+    def set_position(self, system, x, y, z):
+        # Switch to the desired workspace coordinate system
+        self._printer.commands(system)
+
+        # Set new position for the workspace coordinate system
+        self._printer.commands("G92 X{} Y{} Z{}".format(x, y, z))
+
+    def set_system(self, system):
+        # Switch to the desired workspace coordinate system
+        self._printer.commands(system)
+
+    def on_event(self, event, payload):
+        if event == Events.POSITION_UPDATE:
+            # Get the current position
+            x = payload['x']
+            y = payload['y']
+            z = payload['z']
+
+            # Update the position in the frontend
+            self._plugin_manager.send_plugin_message(self._identifier, {
+                'type': 'positionUpdate',
+                'x': x,
+                'y': y,
+                'z': z
+            })
 
 __plugin_name__ = "Coordinate Systems Plugin"
 __plugin_pythoncompat__ = ">=3.7,<4"
--- octoprint_coordinate_systems/static/js/coordinate_systems.js
+++ octoprint_coordinate_systems/static/js/coordinate_systems.js
@@ -1,20 +1,99 @@
 $(function() {
     function CoordinateSystemsViewModel(parameters) {
         var self = this;
-        
+    
         self.control = parameters[0];
-        
+    
         self.gcode = ko.observable('');
         
+        self.activeSystem = ko.observable('G54');
+        self.xOffset = ko.observable(0);
+        self.yOffset = ko.observable(0);
+        self.zOffset = ko.observable(0);
+        self.xPosition = ko.observable(0);
+        self.yPosition = ko.observable(0);
+        self.zPosition = ko.observable(0);
+
+        self.systemLabels = ko.observableArray([]);
+        
+        self.availableSystems = ko.observableArray([
+            'G54',
+            'G55',
+            'G56',
+            'G57',
+            'G58',
+            'G59',
+            'G59.1',
+            'G59.2',
+            'G59.3',
+        ]);
+
+        self.save_offsets = function(system, xOffset, yOffset, zOffset, label) {
+            OctoPrint.postJson('plugin/coordinate_systems/save_offsets', {
+                system: system,
+                x_offset: xOffset,
+                y_offset: yOffset,
+                z_offset: zOffset,
+                label: label
+            });
+        };
+
+        self.restore_offsets = function(system) {
+            OctoPrint.getJson('plugin/coordinate_systems/load_offsets', {
+                system: system
+            }).done(function(response) {
+                var offsets = response.offsets;
+                self.xOffset(offsets.x);
+                self.yOffset(offsets.y);
+                self.zOffset(offsets.z);
+                self.label(offsets.label);
+
+                var systemIndex = self.systemLabels().findIndex(function(element) {
+                    return element.system === system;
+                });
+
+                if (systemIndex !== -1) {
+                    self.systemLabels()[systemIndex].label = offsets.label;
+                } else {
+                    self.systemLabels.push({system: system, label: offsets.label});
+                }
+
+                self.set_offsets(system, offsets.x, offsets.y, offsets.z);
+            });
+        };
+
         self.sendCustomCommand = function(command) {
             var gcode = self.gcode();
             if (!gcode) {
                 return;
             }
-            
+        
             self.control.sendCustomCommand({ command: gcode });
             self.gcode('');
         };
+
+        self.systemOptions = ko.computed(function() {
+            return self.availableSystems().map(function(system) {
+                var label = self.systemLabels().find(function(element) {
+                    return element.system === system;
+                });
+
+                return {
+                    system: system,
+                    text: system + (label ? " (" + label.label + ")" : "")
+                };
+            });
+        });
+        
+        self.activeSystem.subscribe(function(newSystem) {
+            // Switch to the new coordinate system
+            OctoPrint.postJson('plugin/coordinate_systems/set_system', {
+                system: newSystem
+            });
+
+            // Request current position
+            OctoPrint.control.sendGcode('M114');
+        });
 
         self.set_offsets = function(system, xOffset, yOffset, zOffset) {
             // Use OctoPrint's API client library to make a POST request to your plugin's API endpoint
@@ -24,6 +103,27 @@ $(function() {
                 y_offset: yOffset,
                 z_offset: zOffset
             });
+        };
+
+        self.set_position = function(system, x, y, z) {
+            OctoPrint.postJson('plugin/coordinate_systems/set_position', {
+                system: system,
+                x: x,
+                y: y,
+                z: z
+            });
+        };
+
+        self.onDataUpdaterPluginMessage = function(plugin, data) {
+            if (plugin != 'coordinate_systems') {
+                return;
+            }
+
+            if (data.type == 'positionUpdate') {
+                self.xPosition(data.x);
+                self.yPosition(data.y);
+                self.zPosition(data.z);
+            }
         };
     }
     
